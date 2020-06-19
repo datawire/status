e9d9a747ae50ea6b6a2ceb67821b58b03814623a
alex <alex@datawire.io>
2020-06-19 11:54:32 -0400

    initial status page

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # status
+
+Credits to https://codedgeekery.com/build-a-status-page-powered-by-github-and-uptime-robot/
---
+++ script.js
@@ -0,0 +1,79 @@
+$(document).ready(function () {
+    var config = {
+        github: {
+            org: 'alexgervais',
+            repo: 'status'
+        }
+    };
+
+    $.getJSON('https://api.github.com/repos/' + config.github.org + '/' + config.github.repo + '/issues?state=all').done(message);
+
+    function message(issues) {
+        issues.forEach(function (issue) {
+            var status = issue.labels.reduce(function (status, label) {
+                if (/^status:/.test(label.name)) {
+                    return label.name.replace('status:', '');
+                } else {
+                    return status;
+                }
+            }, 'operational');
+
+            var systems = issue.labels.filter(function (label) {
+                return /^system:/.test(label.name);
+            }).map(function (label) {
+                return label.name.replace('system:', '')
+            });
+
+            console.log(status);
+            console.log(systems);
+
+            if (issue.state === 'open') {
+                $('#panel').data('incident', 'true');
+                $('#panel').attr('class', (status === 'operational' ? 'panel-success' : 'panel-warning'));
+                $('#paneltitle').html('<a href="#incidents">' + issue.title + '</a>');
+            }
+
+            var html = '<article class="timeline-entry">\n';
+            html += '<div class="timeline-entry-inner">\n';
+
+            if (issue.state === 'closed') {
+                html += '<div class="timeline-icon bg-success"><i class="entypo-feather"></i></div>';
+            } else {
+                html += '<div class="timeline-icon bg-secondary"><i class="entypo-feather"></i></div>';
+            }
+
+            html += '<div class="timeline-label">\n';
+            html += '<span class="date">' + datetime(issue.created_at) + '</span>\n';
+
+            if (issue.state === 'closed') {
+                html += '<span class="badge label-success pull-right">closed</span>';
+            } else {
+                html += '<span class="badge ' + (status === 'operational' ? 'label-success' : 'label-warning') + ' pull-right">open</span>\n';
+            }
+
+            for (var i = 0; i < systems.length; i++) {
+                html += '<span class="badge system pull-right">' + systems[i] + '</span>';
+            }
+
+            html += '<h2>' + issue.title + '</h2>\n';
+            html += '<hr>\n';
+            html += '<p>' + issue.body + '</p>\n';
+
+            if (issue.state === 'closed') {
+                html += '<p><em>Updated ' + datetime(issue.closed_at) + '<br/>';
+                html += 'The system is back in normal operation.</p>';
+            }
+            html += '</div>';
+            html += '</div>';
+            html += '</article>';
+            $('#incidents').append(html);
+        });
+
+        function datetime(string) {
+            var datetime = string.split('T');
+            var date = datetime[0];
+            var time = datetime[1].replace('Z', '');
+            return date + ' ' + time;
+        };
+    };
+});
