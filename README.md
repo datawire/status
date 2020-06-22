59c05eaf49914873c5d026ab2000497633cc2087
alex <alex@datawire.io>
2020-06-22 11:34:39 -0400

    transferred ownership to datawire org

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # status
 
+This GitHub Pages is hosted at status.getambassador.io
+
 Credits to https://codedgeekery.com/build-a-status-page-powered-by-github-and-uptime-robot/
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     var config = {
         github: {
-            org: 'alexgervais',
+            org: 'datawire',
             repo: 'status'
         }
     };
